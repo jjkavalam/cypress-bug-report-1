a3e393c0eefb9f023a09844d02999d936c486129
Jobin Jacob Kavalam <jjkavalam@gmail.com>
2022-11-17 10:30:16 +0800

    add actual test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,11 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('mocha behavior with multiple errors', function () {
+  beforeEach(function () {
+    throw new Error('error 1')
   })
-})
+  it('sample test', function () {
+
+  })
+  afterEach( function () {
+    throw new Error('error 2')
+  })
+});
